82231e92604fb407af59ca2b1f1c4b0219fcbb76
Tajammul-mandate <tajjmul@mandates.in>
2024-11-27 12:30:01 +0530

    Merge Removed Username

--- core_app/models.py
+++ core_app/models.py
@@ -29,7 +29,6 @@ class UserModel(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True, null=True)
     mobile = models.CharField(max_length=15, blank=True, null=True)
     username = None
-    
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [] 
--- student_app/views.py
+++ student_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Auth API
+class Registration(APIView):
+    serializer_class = CustomUser_serializer
+    
+    @swagger_auto_schema(tags=['Auth APIs'], request_body=serializer_class, operation_description="API for Registration", operation_summary="Registration API")
+    def post(self, request, *args, **kwargs):
+        if 'role' not in request.data or not request.data['role']:
+            request.data['role'] = 'QR_user'
+            
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
