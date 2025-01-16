90f3b427bf339f18025b33e5826d4a0c9df1bd3e
Tajammul-mandate <tajjmul@mandates.in>
2025-01-16 11:51:35 +0530

    cors middleware added

--- Maha_Intern/settings/production.py
+++ Maha_Intern/settings/production.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Cors Header Middleware
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
