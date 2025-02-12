8592fb841c698a2e78bc737aa416864d0f83c7e3
rajeshbhagure99 <rajesh@mandates.in>
2025-02-12 15:10:19 +0530

    taluka and district api added organization registration

--- Maha_Intern/settings/local.py
+++ Maha_Intern/settings/local.py
@@ -115,12 +115,6 @@ WSGI_APPLICATION = 'Maha_Intern.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 DATABASES = {
--- admin_app/models.py
+++ admin_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class District(models.Model):
+    district = models.CharField(max_length=155,null=True,blank=True)
+    class Meta:
+        app_label = 'admin_app'
+        db_table = 'District'
+        verbose_name = 'District'
+        verbose_name_plural = 'Districts'
+
+class Taluka(models.Model):
+    district = models.ForeignKey('admin_app.District', on_delete=models.CASCADE, null=True,blank=True)
+    taluka =  models.CharField(max_length=155,null=True,blank=True)
+
+    class Meta:
+        app_label = 'admin_app'
+        db_table = 'Taluka'
+        verbose_name = 'Taluka'
+        verbose_name_plural = 'Talukas'
